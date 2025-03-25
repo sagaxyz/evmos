f724ca2dabc096cf9fa6d53045aba7215febf63b
Ashish Chandra <ashish@saga.xyz>
2025-03-24 19:29:47 -0500

    Added flag to enable/disable setting of gas refund percent

--- x/evm/keeper/state_transition.go
+++ x/evm/keeper/state_transition.go
@@ -328,8 +328,12 @@ func (k *Keeper) ApplyMessageWithConfig(
 	v := ctx.Value("fee-payer")
 	if v != nil {
 		m_params := k.GetParams(ctx)
-		if m_params.GasRefundPercent >= 1 && m_params.GasRefundPercent <= 100 {
-			refundQuotient = uint64(100 / m_params.GasRefundPercent)
+		if m_params.EnableSetGasRefundPercent {
+			if m_params.GasRefundPercent >= 1 && m_params.GasRefundPercent <= 100 {
+				refundQuotient = uint64(100 / m_params.GasRefundPercent)
+			} else {
+				refundQuotient = params.RefundQuotient
+			}
 		} else {
 			refundQuotient = params.RefundQuotient
 		}
